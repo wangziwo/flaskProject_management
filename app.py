7162f85125e2a7dff5482193bd31904658fdb4e7
wangziwo <54460457+wangziwo@users.noreply.github.com>
2020-11-04 15:19:40 +0800

    init

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, flash
+#导入wtf扩展的表单类
+from flask_wtf import FlaskForm
+#导入自定义表单需要的字段
+from wtforms import SubmitField,StringField,PasswordField
+#导入wtf扩展提供的表单验证器
+from wtforms.validators import DataRequired,EqualTo
+from db import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='heima'
+# 更改代码立刻显示更新
+app.DEBUG=True
+# 更改模板立刻显示更新
+app.jinja_env.auto_reload = True
+# 解决编码问题
+# import sys
+# reload(sys)
+# sys.setdefaultencoding("utf-8")
+
+# 需要自定义一个表单类
+class RegisterForm(FlaskForm):
+    username = StringField('学号:', validators=[DataRequired()])
+    password = PasswordField('密码:', validators=[DataRequired()])
+    # password2 = PasswordField('确认密码:', validators=[DataRequired(), EqualTo('password', '密码输入不一致')])
+    submit = SubmitField('登陆')
+# 登陆界面
+@app.route('/', methods=['GET', 'POST'])
+def log_in():
+    register_form = RegisterForm()
+    if request.method == 'POST':
+        # 调用validate_on_submit方法, 可以一次性执行完所有的验证函数的逻辑
+        if register_form.validate_on_submit():
+            # 进入这里就表示所有的逻辑都验证成功
+            username = request.form.get('username')
+            password = request.form.get('password')
+            if password_verify(username=username,password=password)==True:
+                return 'success'
+            else:
+                return '密码错误'
+
+
+
+        else:
+            # message = register_form.errors.get('password2')[0]
+            # flash(message)
+            flash('参数有误')
+
+    return render_template('login.html', form=register_form)
+
+
+@app.route('/<num>')
+def get_hello_world(num):
+    # return 'Hello World! %s' % num
+    url_1 = 'www.baidu.com'
+    my_list = [1,3,5,9]
+    return render_template('index.html',
+                           url_1=url_1,
+                           my_list = my_list
+                           )
+
+if __name__ == '__main__':
+    app.run()
---
+++ db.py
@@ -0,0 +1,43 @@
+import pymysql
+
+# user = input('请输入用户名：')
+# pwd = input('请输入密码：')
+
+# 1.连接
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='test', charset='utf8')
+# 2.创建游标
+cursor = conn.cursor()
+#
+# #注意%s需要加引号
+# # sql = "select * from t1.userinfo where username='%s' and pwd='%s'" %(user, pwd)
+# username = '1'
+# sql = "select name from stu where stu_id='%s'"%(username)
+# print(sql)
+#
+# # 3.执行sql语句
+# cursor.execute(sql)
+#
+# result=cursor.fetchone() #执行sql语句，返回sql查询成功的记录数目
+# print(result[0],type(result[0]))
+#
+# # 关闭连接，游标和连接都要关闭
+# cursor.close()
+# conn.close()
+#
+# if result:
+#     print('登陆成功')
+# else:
+#     print('登录失败')
+# 密码验证函数
+def password_verify(username,password):
+    sql = "select name from stu where stu_id='%s'" % (username)
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if password == result[0]:
+        print(1)
+        return True
+    else:
+        print(0)
+        return False
+if __name__ == '__main__':
+    password_verify(1,'wang')
